c43f2083cdd6f5618d5cd105211d8ef637a237e0
youwenda <youwenda@163.com>
2021-04-27 02:51:48 +0800

    feat: 增加setEnv处理

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const taojimu = requirePlugin('taojimu');
 App({
   // app 全局数据
   globalData: {
@@ -16,6 +17,54 @@ App({
       }
     }
     this.globalData.pageConf = pageConf;
+    taojimu.setEnv({
+      cid: 'test', // 当前游戏的唯一标志，全局唯一，应该避免和其他游戏isv商冲突。所有的cid 公用一个值即可。主要避免和其他isv重复。
+      pageId: 123456, // 页面id, 由淘积木负责生成。测试时可随便制定，用于和cid 一起区分不同isv 厂商在不同页面下游戏和任务的消耗情况。
+      tasks: [
+        {
+          taskName: 'favorShop',
+          addChance: 1,
+          maxCount: 1,
+          isDaily: false,
+        },
+        {
+          taskName: 'favorItem',
+          addChance: 1,
+          maxCount: 1,
+          isDaily: true,
+        },
+        {
+          taskName: 'addBag',
+          addChance: 1,
+          maxCount: 1,
+          isDaily: true,
+        },
+        {
+          taskName: 'goShop',
+          addChance: 1,
+          maxCount: 1,
+          isDaily: true,
+        },
+        {
+          taskName: 'appointLive',
+          addChance: 1,
+          maxCount: 1,
+          isDaily: true,
+        },
+        {
+          taskName: 'joinMember',
+          addChance: 1,
+          maxCount: 1,
+          isDaily: false,
+        },
+        {
+          taskName: 'share',
+          addChance: 1,
+          maxCount: 1,
+          isDaily: true,
+        },
+      ],
+    });
   },
   onShow(options) {
     // 从后台被 scheme 重新打开
