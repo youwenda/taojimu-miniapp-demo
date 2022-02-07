b40614117f1965493103ac4e4b0bf983ff49dd77
麦梓 <maizi.wxx@alibaba-inc.com>
2022-02-07 18:54:08 +0800

    feat: 使用开放UI组件搭建demo

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,53 +1,24 @@
-const taojimu = requirePlugin('taojimu');
 Page({
-  onLoad(query) {
-    // 页面加载
-    console.info(`Page onLoad with query: ${JSON.stringify(query)}`);
+  data: {},
+  onLoad() {
     this.setData({
-      component: getApp().globalData.pageConf.componentTree[0]
+      visible: true,
+      target: 'home',
+      isFullScreen: true,
+      gameIntro: {
+        'src': 'https://lego.alicdn.com/res/tjm/v/2247603103_9tJGcNkO18ExnKRf_sd.mp4',
+        'width': 600,
+        'height': 480,
+        duration: '3.804000',
+        poster: 'https://lego.alicdn.com/res/tjm/p/2247603103_9tJGcNkO18ExnKRf_snapshot.jpg',
+      },
     });
-
-    setTimeout(() => {
-      taojimu.log.sendHdLog({ 
-        status: 'finished'
-      }).then(() => {
-        my.showToast({ 
-          content: '3s后领取喵币成功'
-        })
-      });
-    }, 3e3);
-
   },
-  onReady() {},
-  onShow() {
-    // 页面显示
-  },
-  onHide() {
-    // 页面隐藏
-  },
-  onUnload() {
-    // 页面被关闭
-  },
-  onTitleClick() {
-    // 标题被点击
-  },
-  onPullDownRefresh() {
-    // 页面被下拉
-  },
-  onReachBottom() {
-    // 页面被拉到底部
-  },
-  onShareAppMessage() {
-    // 返回自定义分享信息
-    return {
-      title: "Demo-MMGame",
-      desc: "Demo-MMGame",
-      path: "pages/mmgame/index",
-    };
-  },
-  onNavigateToTask() {
+  onGameStart() {
     my.navigateTo({
-      url: '/pages/task/task'
+      url: '/pages/game/index'
     });
-  }
+  },
+  onShowRule() {},
+  onShowPrize() {},
 });
